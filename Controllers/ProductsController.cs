ed87fdada988d2d6a952003be412e47281979780
Kurban KOTAN <kurbankotan@gmail.com>
2024-08-22 14:43:15 +0300

    ApiController

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductsAPI.Controllers
+{
+    //localhost:7201/api/products
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController:ControllerBase
+    {
+        private static readonly string[] Products =
+        {
+            "IPhone 14", "IPhone 15","IPhone 16"
+        };
+
+        //localhost:7201/api/products => GET
+        [HttpGet]
+        public string[] GetProducts()
+        {
+            return Products;
+        }
+
+
+        //localhost:7201/api/products/1 => GET
+
+        [HttpGet("{id}")]                                 //[HttpGet("api/[controller]/{id}")] şeklinde de yazılabilir
+        public string GetProducts(int id)
+        {
+            return Products[id];
+        }
+    }
+}
