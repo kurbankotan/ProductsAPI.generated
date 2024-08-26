b2b7d55e5e066f4e6b172fb93e62a840640590a1
Kurban KOTAN <kurbankotan@gmail.com>
2024-08-26 15:12:05 +0300

    Adding Entity FrameWork

---
+++ Models/ProductsContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductsAPI.Models
+{
+    public class ProductsContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ProductsContext(DbContextOptions<ProductsContext> options):base(options)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductID = 1, ProductName = "IPhone 14", Price = 60000, IsActive = true });
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductID = 2, ProductName = "IPhone 15", Price = 70000, IsActive = true });
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductID = 3, ProductName = "IPhone 16", Price = 80000, IsActive = false });
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductID = 4, ProductName = "IPhone 17", Price = 90000, IsActive = true });
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductID = 5, ProductName = "IPhone 18", Price = 100000, IsActive = true });
+        }
+
+
+
+
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ProductsAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
+builder.Services.AddDbContext<ProductsContext>(x => x.UseSqlite("Data Source = products.db"));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
